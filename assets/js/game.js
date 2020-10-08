2283fafb63e5f5fbd78fa1cd4fb2fa8c25de6aa8
James Costello <jcotab@gmail.com>
2020-10-07 17:08:44 -0700

    looped through array and fought each bot

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,11 +6,11 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ['Roberto', 'Amy Andriod', 'Robo Trumble'];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
 // Alert users that they are starting the round
 window.alert("Welcome to Robot Gladiators!");
 var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose');
@@ -61,4 +61,6 @@ if (playerHealth <= 0) {
 
 };
 
-fight();
+for ( let i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
